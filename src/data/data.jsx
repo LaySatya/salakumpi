661b134711f6a7a1fe0fd219bf8d99d19995c4f9
LaySatya <141922646+LaySatya@users.noreply.github.com>
2025-03-26 02:55:43 +0700

    add-teacher-name-and-bible

--- src/data/data.jsx
+++ src/data/data.jsx
@@ -2,363 +2,430 @@ export const questions = [
     // អែសរ៉ា
 
 
-    // គ្រូសុហេង
+    // heng
     {
         question: "តើពួកសាសន៍យូដាបានអាស្រ័យនៅក្រុងបាប៊ីឡូនរយៈពេលប៉ុន្មានឆ្នាំ?",
         answer: "៧០ ឆ្នាំ",
-        type: 1  // represented esra questions
+        type: 1, // represented esra questions
+        teacher: "heng"
     },
     {
         question: "ស្ដេចឈ្មោះអ្វី ដែលបានអនុញ្ញាតឲពួកយូដាវិលទៅអីស្រាអែលវិញ?",
         answer: "ស៊ីរូស ស្ដេចពើស៊ី",
-        type: 1  // represented esra questions
+        type: 1,  // represented esra questions
+        teacher: "heng"
     },
     {
         question: "នៅពេលទៅដល់ហើយ តើពួកគេចាប់ផ្ដើមធ្វើអ្វីមុនគេ?",
         answer: "ស្អាងអាសនា ថ្វាយបង្គំ",
-        type: 1  // represented esra questions
+        type: 1,  // represented esra questions
+        teacher: "heng"
     },
     {
         question: "តើពួកទាំងនោះធ្វើអ្វីទៀត បន្ទាប់ពីស្អាងអាសនារួច?",
         answer: "សាងសង់ព្រះវិហារ",
-        type: 1  // represented esra questions
+        type: 1,  // represented esra questions
+        teacher: "heng"
     },
     {
         question: "តើអ្នកណាជាអ្នកដឹកនាំបណ្ដាជនត្រឡប់ពីបាប៊ីឡូនវិញ?",
         answer: "សូរ៉ូបាបិល និងយេសួរ",
-        type: 1  // represented esra questions
+        type: 1,  // represented esra questions
+        teacher: "heng"
     },
     {
         question: "តើមានអ្វីកើតឡើង កាលពួកជាងកំពុងតែដាក់ជើងជញ្ជាំងព្រះវិហារឡើង?",
         answer: "មានអ្នកខ្លះ បានស្រែកហោរដោយអរសប្បាយ ហើយខ្លះទៀតយំ",
-        type: 1  // represented esra questions
+        type: 1,  // represented esra questions
+        teacher: "heng"
     },
     {
         question: "តើអ្នកណាដែលបានមកបង្កបញ្ហាដល់ពួកសាសន៍យូដា?",
         answer: "ពួកសាសន៍សាម៉ារី",
-        type: 1  // represented esra questions
-    }, {
+        type: 1,  // represented esra questions
+        teacher: "heng"
+    },
+    {
         question: "ស្ដេចដារីយុស បានចេញបង្គាប់ថា បើអ្នកណាបំផ្លាស់បំប្រែប្រកាសនេះនឹងត្រូវមានទោសបែបណា?",
         answer: "ដោះធ្នឹម១ពីផ្ទះអ្នកនោះបញ្ឈរឡើងចងកគេ ផ្ទះគេទុកសំរាប់បន្ទោលាមក",
-        type: 1  // represented esra questions
+        type: 1,  // represented esra questions
+        teacher: "heng"
     },
     {
         question: "តើព្រះវិហារយេរូសាឡិមត្រូវបានសាងសង់ក្នុងរាជ្យស្ដេចណាខ្លះ?",
         answer: "ស៊ីរូស ដារីយុស និងអើថាស៊ើកសេស ជាស្ដេចពើស៊ី",
-        type: 1  // represented esra questions
+        type: 1,  // represented esra questions
+        teacher: "heng"
     },
     {
         question: "តើមានបញ្ហាអ្វីនៅក្នុងចំណោមបណ្ដាជន?",
         answer: "ការរៀបការជាមួយសាសន៍ដទៃ",
-        type: 1  // represented esra questions
+        type: 1,  // represented esra questions
+        teacher: "heng"
     },
     {
         question: "ពេលអែសរ៉ាបានឮពីការរៀបការជាមួយសាសន៍ដទៃ តើគាត់ធ្វើបែបណា?",
         answer: "ហែកសំលៀកបំពាក់ បោចសក់ក្បាល និងពុកចង្ការរបស់ខ្លួន ហើយស្លុតចិត្តអង្គុយនៅ",
-        type: 1  // represented esra questions
+        type: 1,  // represented esra questions
+        teacher: "heng"
     },
 
-    // គ្រូសុផុន
+    // phon
     {
         question: "តើមានអ្វីកើតឡើង នៅក្នុងឆ្នាំទី១ នៃស្ដេចស៊ីរុសជាស្ដេចពើស៊ី?",
         answer: "មានបញ្ជាអនុញ្ញាត្ត អោយសាសន៍អ៊ីស្រាអែលណាដែលចង់វិលត្រឡប់ទៅ ក្រុងយេរូសាឡិមវិញ ដើម្បីធ្វើព្រះវិហារនោះអោយគេទៅចុះ ព្រមទាំងជួយផ្គត់ផ្គង់ដល់ការសាងសង់ផង",
-        type: 1  // represented esra questions
+        type: 1,  // represented esra questions
+        teacher: "phon"
     },
     {
         question: "តើនរណាខ្លះដែលបាន ផ្ដើមគំនិតធ្វើព្រះវិហារឡើងវិញ?",
         answer: "សូរ៉បាបិល និងយេសួរ",
-        type: 1  // represented esra questions
+        type: 1,  // represented esra questions
+        teacher: "phon"
     },
     {
         question: "តើព្រះវិហារបានផ្អាកធ្វើ ដល់ឆ្នាំទីប៉ុន្មាននៃរាជ្យស្ដេចណា?",
         answer: "ផ្អាកដល់ឆ្នាំទី២នៃរាជ្យ​ ស្ដេចដារីយូសជាស្ដេចពើស៊ីទើបចាប់ធ្វើបន្តទៀត",
-        type: 1  // represented esra questions
+        type: 1,  // represented esra questions
+        teacher: "phon"
     },
     {
         question: "តើនរណាខ្លះដែលរារាំង ក្នុងការធ្វើព្រះវិហារ?",
         answer: "ថាថ្នាយ, សេថារ-បូសណាយ​ និងគូកនរបស់គាត់",
-        type: 1  // represented esra questions
+        type: 1,  // represented esra questions
+        teacher: "phon"
     },
     {
         question: "តើអែសរ៉ាជាជំនាន់ក្រោយរបស់អ្នកណា?",
         answer: "ជំនាន់ក្រោយរបស់អឺរ៉ុន",
-        type: 1  // represented esra questions
+        type: 1,  // represented esra questions
+        teacher: "phon"
     },
     {
         question: "តើអែសរ៉ាមានការងារជាអ្វី?",
         answer: "គាត់មានការងារជាស្មៀន",
-        type: 1  // represented esra questions
+        type: 1,  // represented esra questions
+        teacher: "phon"
     },
     {
         question: "តើការអ្វីដែលនាំអោយអែសរ៉ាស្លុតចិត្ត?",
         answer: "ពួកអ៊ីស្រាអែល ពួកសង្ឃ និងពួកលេវី បានយកស្រ្ដីសាសន៍ដទៃមកធ្វើប្រពន្ធ",
-        type: 1  // represented esra questions
+        type: 1,  // represented esra questions
+        teacher: "phon"
     },
 
-    // គ្រូខុម
+    // khom
     {
         question: "តើស្ដេចឈ្មោះអ្វី? នៃប្រទេសណា? ដែលបានបង្គាប់អោយគេស្អាងព្រះវិហារឡើងវិញ?",
         answer: "ស្ដេចស៊ីរូស នៃប្រទេសពើស៊ី",
-        type: 1  // represented esra questions
+        type: 1,  // represented esra questions
+        teacher: "khom"
     },
     {
         question: "តើបណ្ដាជនមានចិត្តមូលគ្នាតែមួយនៅកន្លែងណា?",
         answer: "ក្រុងយេរូសាឡិម",
-        type: 1  // represented esra questions
+        type: 1,  // represented esra questions
+        teacher: "khom"
     },
     {
         question: "នៅពេលចាប់តាំងដាក់ជើងជញ្ជាំង តើបណ្ដាជនគេបាននាំគ្នាច្រៀងឆ្លើងឆ្លងគ្នាថាយ៉ាងដូចម្ដេច?",
         answer: "ទ្រង់ប្រកបដោយករុណាគុណ​ សេចក្តីសប្បុរសនៃទ្រង់ស្ថិតស្ថេរនៅអស់កល្បជានិច្ច",
-        type: 1  // represented esra questions
+        type: 1,  // represented esra questions
+        teacher: "khom"
     },
     {
         question: "ចូររៀបរាប់ឈ្មៅស្ដេចពើស៊ីតាមដំណាក់កាល?",
         answer: "ស៊ីរូស ដារីយុស អ័ហាស៊ូរុស និងអើថាសើកសេស",
-        type: 1  // represented esra questions
+        type: 1,  // represented esra questions
+        teacher: "khom"
     },
     {
         question: "តើព្រះវិហារដែលស្ដេចស៊ីរូសបានបង្គាប់អោយសាងសង់មានកម្ពស់ប៉ុន្មាន? និងទទឹងប៉ុន្មាន?",
         answer: "មានកម្ពស់៦០ហត្ថ ទទឹង៦០ហត្ថ",
-        type: 1  // represented esra questions
+        type: 1,  // represented esra questions
+        teacher: "khom"
     },
     {
         question: "តើព្រះវិហារត្រូវបានស្អាងឡើងរួចជាស្រេចវិញនៅក្នុងរាជ្យស្ដេចអង្គណានៃស្ដេចពើស៊ី?",
         answer: "ដារីយុស",
-        type: 1  // represented esra questions
+        type: 1,  // represented esra questions
+        teacher: "khom"
     },
     {
         question: "តើលោកអែសរ៉ាជាស្មៀនដែលស្ទាត់ជំនាញខាងក្រិត្យវិន័យរបស់អ្នកណា?",
         answer: "លោកម៉ូសេ",
-        type: 1  // represented esra questions
+        type: 1,  // represented esra questions
+        teacher: "khom"
     },
 
 
 
     // នេហេមា
-    
-    // គ្រូសុហេង
+
+    // heng
     {
         question: "តើនេហេមា ជាអ្នកណា?",
         answer: "អ្នកថ្វាយពែងដល់ស្ដេច",
-        type: 2  // represented nehemea questions
+        type: 2,  // represented nehemea questions
+        teacher: "heng"
     },
     {
         question: "តើនេហេមា បានធ្វើអ្វីបន្ទាប់ពីបានឭថា កំផែងក្រុងត្រូវបំផ្លាញ?",
         answer: "អង្គុយយំ ហើយសៅសោកនៅអស់ពីរបីថ្ងៃ ក៍តម ហើយអធិស្ធាន",
-        type: 2  // represented nehemea questions
+        type: 2,  // represented nehemea questions
+        teacher: "heng"
     },
     {
         question: "តើពួកណាខ្លះ ដែលបានព្យាមយាមរារាំងការសាងសង់កំផែងឡើងវិញ?",
         answer: "សានបាឡាត ជាអ្នកស្រុកហូរ៉ូណែម និងថូប៊ីយ៉ា សាសន៍អាំម៉ូន ហើយកេសែម ជាសាសន៍អារ៉ាប់",
-        type: 2  // represented nehemea questions
+        type: 2,  // represented nehemea questions
+        teacher: "heng"
     },
     {
         question: "តើពួកគេចំអកដល់កំផែងដែលពួកយូដាបានសាងសង់ថាម៉េច?",
         answer: "បើគ្រាន់តែមានចចមួយឡើងទៅលើកំផែងថ្ម ដែលគេកំពុងតែធ្វើនោះរលំទៅហើយ",
-        type: 2  // represented nehemea questions
+        type: 2,  // represented nehemea questions
+        teacher: "heng"
     },
     {
         question: "តើពួកអ្នកធ្វើកំផែង គេធ្វើការបែបណា?",
         answer: "ធ្វើការដោយដៃម្ខាង ដៃម្ខាងទៀតកាន់គ្រឿងសស្រា្តវុធ",
-        type: 2  // represented nehemea questions
+        type: 2,  // represented nehemea questions
+        teacher: "heng"
     },
     {
         question: "តើកំផែងទីក្រុងយេរូសាឡិមបានធ្វើរួចក្នុងរយះពេលប៉ុន្មាន?",
         answer: "៥២ ថ្ងៃ",
-        type: 2  // represented nehemea questions
+        type: 2,  // represented nehemea questions
+        teacher: "heng"
     },
 
-    // គ្រូសុផុន
+    // phon
     {
         question: "តើនេហេមា ធ្វើជាអ្នកថ្វាយពែង បម្រើដល់ស្ដេចអង្គណា?",
         answer: "អើថាសើកសេស",
-        type: 2  // represented nehemea questions
+        type: 2,  // represented nehemea questions
+        teacher: "phon"
     },
-     {
+    {
         question: "តើនរណាប្រាប់នេហេមាថាពួកយូដាត្រូវចាប់ទៅជាឈ្លើយ​ ហើយក្រុងយេរូសាឡិម ត្រូវបំផ្លាញអស់?",
         answer: "ហាណានី",
-        type: 2  // represented nehemea questions
+        type: 2,  // represented nehemea questions
+        teacher: "phon"
     },
     {
         question: "តើនេហេមាទៅធ្វើអ្វីនៅក្នុងយេរូសាឡិម?",
         answer: "ធ្វើកំផែងក្រុងយេរូសាឡិម",
-        type: 2  // represented nehemea questions
+        type: 2,  // represented nehemea questions
+        teacher: "phon"
     },
     {
         question: "តើនរណាខ្លះដែលរំខាននេហេមា ក្នុងការជួសជុលកំផែងក្រុងឡើងវិញ?",
         answer: "សានបាឡាត, ថូប៊ីយ៉ា",
-        type: 2  // represented nehemea questions
+        type: 2,  // represented nehemea questions
+        teacher: "phon"
     },
     {
         question: "តើកំផែងក្រុងយេរូសាឡិមជួសជុលហើយក្នុងរយះពេលប៉ុន្មានថ្ងៃ?",
         answer: "៥២ ថ្ងៃ",
-        type: 2  // represented nehemea questions
+        type: 2,  // represented nehemea questions
+        teacher: "phon"
     },
     {
         question: "តើនេហេមាធ្វើជាចៅហ្វាយនៅស្រុកយូដាអស់ប៉ុន្មានឆ្នាំ?",
         answer: "១២ ឆ្នាំ",
-        type: 2  // represented nehemea questions
+        type: 2,  // represented nehemea questions
+        teacher: "phon"
     },
     {
         question: "តើសង្ឃមួយណាដែលភ្ជាប់ញាតិនឺងថូប៊ីយ៉ា?",
         answer: "អេលីយ៉ាស៊ីប",
-        type: 2  // represented nehemea questions
+        type: 2,  // represented nehemea questions
+        teacher: "phon"
     },
 
-    // គ្រូខុម
+    // khom
     {
         question: "នៅពេលដែលនេហេមាបានដឹងដំណឹងថាកំផែងក្រុងយេរូសាឡិមបានបាក់បែក តើគាត់បានធ្វើអ្វី?",
         answer: "គាត់បានអង្គុយយំ សៅសោកអស់ ២ ៣ថ្ងៃ តមអត់ និងអធិស្ធាន",
-        type: 2  // represented nehemea questions
+        type: 2,  // represented nehemea questions
+        teacher: "khom"
     },
     {
         question: "ពួកសាសន៍យូដាកំពុងសាងសង់កំផែងឡើងវិញ តើអ្នកណាមានចិត្តឡកឡឺយដល់គេ?",
         answer: "សានបាឡាត",
-        type: 2  // represented nehemea questions
+        type: 2,  // represented nehemea questions
+        teacher: "khom"
     },
     {
         question: "ថូប៊ីយ៉ាដែលជាអ្នកប្រឆាំងនិងការសាងសង់កំផែងឡើងវិញ​ តើជាសាសន៍អ្វី?",
         answer: "សាសន៍អាំម៉ូន",
-        type: 2  // represented nehemea questions
+        type: 2,  // represented nehemea questions
+        teacher: "khom"
     },
     {
         question: "តើអ្នកណាខ្លះដែលបានហៅនេហេមាទៅពិគ្រោះគ្នារឿងសាងសង់ព្រះវិហារ?",
         answer: "សាបាឡាននិងកេសែម",
-        type: 2  // represented nehemea questions
+        type: 2,  // represented nehemea questions
+        teacher: "khom"
     },
     {
         question: "តើកំផែងក្រុងយេរូសាឡិមបានជួសជុលឡើងវិញអស់រយះពេលប៉ុន្មាន?",
         answer: "៥២ ថ្ងៃ",
-        type: 2  // represented nehemea questions
+        type: 2,  // represented nehemea questions
+        teacher: "khom"
     },
     {
         question: "តើសាសន៍ណាខ្លះដែលត្រូវបានហាម​ មិនអោយចូលទៅក្នុងពួកជំនុំនៃព្រះឡើយ?",
         answer: "ម៉ូអាប់ អាំម៉ូន",
-        type: 2  // represented nehemea questions
+        type: 2,  // represented nehemea questions
+        teacher: "khom"
     },
     {
         question: "តើកេសែមជាសាសន៍អ្វី?",
         answer: "សាសន៍អារ៉ាប់",
-        type: 2  // represented nehemea questions
+        type: 2,  // represented nehemea questions
+        teacher: "khom"
     },
 
     // នាងអែសធើ
 
-    // គ្រូសុខហេង
+    // heng
     {
         question: "តើកណ្ឌគម្ពីរមួយណាដែលអត់មាននិយាយអំពីព្រះជាម្ចាស់?",
         answer: "កណ្ឌ អែសធើរ",
-        type: 3  // represented ester questions
+        type: 3,  // represented ester questions
+        teacher: "phon"
     },
     {
         question: "ក្រោយពីទទួលបានការអនុញ្ញាត សាសន៍យូដាទាំងអស់បាននាំគ្នាត្រឡប់ទៅវិញ តែមានមនុស្សពីរនាក់ ក្នុងចំណោមពួកយូដាផ្សេងទៀតមិនបានត្រឡប់ទៅវិញទេ តើជាអ្នកណា?",
         answer: "ម៉ាដេកាយ និងអែសធើរ",
-        type: 3  // represented ester questions
+        type: 3,  // represented ester questions
+        teacher: "phon"
     },
     {
         question: "តើឈ្មោះ១ទៀត របស់អែសធើរ ជាអ្វី?",
         answer: "ហាដាសា",
-        type: 3  // represented ester questions
+        type: 3,  // represented ester questions
+        teacher: "phon"
     },
     {
         question: "តើភ្នាក់ងារ ២នាក់ ដែលចង់ធ្វើឃាតស្ដេចអ៍ហាស៊ូរុសឈ្មោះអ្វីខ្លះ?",
         answer: "ប៊ីកថាន និងថេរែស",
-        type: 3  // represented ester questions
+        type: 3,  // represented ester questions
+        teacher: "phon"
     },
     {
         question: "តើអ្នកណាដែលជាខ្មាំងសត្រូវរបស់ម៉ាដេកាយ និងសាសន៍យូដា?",
         answer: "ហាម៉ាន",
-        type: 3  // represented ester questions
+        type: 3,  // represented ester questions
+        teacher: "phon"
     },
     {
         question: "តើអ្នកណាដែលហាម៉ានចង់បំផ្លាញ?",
         answer: "សាសន៍យូដា ទាំងអស់",
-        type: 3  // represented ester questions
+        type: 3,  // represented ester questions
+        teacher: "phon"
     },
     {
         question: "តើហាម៉ាន បានប្រគល់ប្រាក់ប៉ុន្មានដល់ស្ដេច ដើម្បីបំផ្លាញសាសន៍យូដា?",
         answer: "១មុឺនហាប",
-        type: 3  // represented ester questions
+        type: 3,  // represented ester questions
+        teacher: "heng"
     },
     {
         question: "នៅពេលឈ្នះលើខ្មាំងសត្រូវហើយ តើពួកគេប្រារព្ធធ្វើពិធីបុណ្យអ្វី?",
         answer: "បុណ្យពោរ",
-        type: 3  // represented ester questions
+        type: 3,  // represented ester questions
+        teacher: "phon"
     },
 
-    // គ្រូសុផុន
+    // phon
 
     {
         question: "តើស្ដេចអ័ហាស៊ូរុស សោយរាជ្យលើប៉ុន្មានខេត្ត?",
         answer: "១២៧ខេត្ត",
-        type: 3  // represented ester questions
+        type: 3,  // represented ester questions
+        teacher: "phon"
     },
     {
         question: "តើនរណាដែលបានទទួលការតែងតាំងអោយធ្វើជាអគ្គមហេសីជំនួសព្រះនាង វ៉ាសធី?",
         answer: "អេសធើ",
-        type: 3  // represented ester questions
+        type: 3,  // represented ester questions
+        teacher: "phon"
     },
     {
         question: "តើម៉ាដេកាយនិងអេសធើ ត្រូវជាអ្វីនឹងគ្នា?",
         answer: "បងប្អុនជីដូនមួយ",
-        type: 3  // represented ester questions
+        type: 3,  // represented ester questions
+        teacher: "phon"
     },
     {
         question: "តើអ្នកដែលចង់សំលាប់ជនជាតិអ៊ីស្រាអែលផ្ដាច់ពូជ មានឈ្មោះអ្វី?",
         answer: "ហាម៉ាន",
-        type: 3  // represented ester questions
+        type: 3,  // represented ester questions
+        teacher: "phon"
     },
     {
         question: "តើបុណ្យដែលបានកំណត់ក្នងការសំលាប់ជនជាតិអ៊ីស្រាអែល ឈ្មោះបុណ្យអ្វី?",
         answer: "បុណ្យពោរ",
-        type: 3  // represented ester questions
+        type: 3,  // represented ester questions
+        teacher: "phon"
     },
     {
         question: "តើហាម៉ានមានកូនប៉ុន្មាននាក់?",
         answer: "១០នាក់",
-        type: 3  // represented ester questions
+        type: 3,  // represented ester questions
+        teacher: "phon"
     },
     {
         question: "តើឈ្មោះមួយទៀតតបស់អេសធើឈ្មោះអ្វី?",
         answer: "ហាដាសា",
-        type: 3  // represented ester questions
+        type: 3,  // represented ester questions
+        teacher: "phon"
     },
 
-    // គ្រូខុម
+    // khom
 
     {
         question: "តើស្ដេចមានឈ្មោះអ្វី? ហើយសោយរាជ្យលើប៉ុន្មានខេត្ត?",
         answer: "ដែលចាប់តាំងពីស្រុកក្លិង្គរហូតអេធីយ៉ូពី អ័ហាសូរូស សោយរាជ្យ១២៧ខេត្ត",
-        type: 3  // represented ester questions
+        type: 3,  // represented ester questions
+        teacher: "khom"
     },
     {
         question: "តើព្រះនាងឈ្មោះអ្វីដែលបានបដិសេធការអញ្ជើញរបស់ស្ដេចចូលរួមកម្មវិធី?",
         answer: "វ៉ាសធី",
-        type: 3  // represented ester questions
+        type: 3,  // represented ester questions
+        teacher: "khom"
     },
     {
         question: "នៅក្រុងស៊ូសានមានសាសន៍យូដាម្នាក់ឈ្មោះអ្វីដែលជាពូជបេនយ៉ាមីន?",
         answer: "ម៉ាដេកាយ",
-        type: 3  // represented ester questions
+        type: 3,  // represented ester questions
+        teacher: "khom"
     },
     {
         question: "តើព្រះនាងអែសធើមានឈ្មោះមួយផ្សេងទៀតហៅថាអ្វី?",
         answer: "ហាដាសា",
-        type: 3  // represented ester questions
+        type: 3,  // represented ester questions
+        teacher: "khom"
     },
     {
         question: "តើស្ដេចអ័ហាសូរុសបានលើកដំកើងមនុស្សម្នាក់ដែលមានឈ្មោះដាអ្វី?ជាកូនអ្នកណា?សាសន៍?",
         answer: "ហាម៉ាន ជាកូនអាំម្ដាថា សាសន៍អ័កាក់",
-        type: 3  // represented ester questions
+        type: 3,  // represented ester questions
+        teacher: "khom"
     },
     {
         question: "តើពួកសាសន៍យូដាបានការពារខ្លួនដោយសំលាប់មនុស្សអស់ប៉ុន្មាននាក់?",
         answer: "៧មុឺន៥ពាន់នាក់",
-        type: 3  // represented ester questions
+        type: 3,  // represented ester questions
+        teacher: "khom"
     },
 
     // កិច្ចការ
-    
+
     // គ្រូសុខហេង
-    
+
 ];
--- src/pages/questions.jsx
+++ src/pages/questions.jsx
@@ -31,28 +31,80 @@ const Questions = () => {
         setShowAnswer(false); // Hide answer when moving to previous question
     };
 
+    const getTeacherName = (name) => {
+        switch (name) {
+            case "heng":
+                return "ហ៊ួរ សុខហេង"
+            case "phon":
+                return "ហាន សុផុន"
+            case "khom":
+                return "ខាន់ សុខុម"
+            default:
+                return "មិនស្គាល់"
+
+        }
+    }
+    const getBible = (bible) => {
+        switch (bible) {
+            case 1:
+                return "អែសរ៉ា"
+            case 2:
+                return "នេហេមា"
+            case 3:
+                return "អេសធើរ"
+            case 4:
+                return "កិច្ចការ"
+            case 5:
+                return "អែសរ៉ាំ, នេហេមា, អេសធើរ, កិច្ចការ"
+            default:
+                return "មិនស្គាល់"
+
+        }
+    }
+
     return (
         <>
             <div className="w-screen h-screen bg-gray-800">
                 <div className="navbar flex justify-between">
                     {/* <div className=""> */}
-                        <Link to={"/options"}>
-                            <a className="btn btn-primary btn-md text-md">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3" />
-                                </svg>
-                                ត្រឡប់
-                            </a>
-                        </Link>
-                        <button onClick={() => setShowAnswer(true)} className="btn btn-success btn-ghost btn-outline text-white">
-                            ចម្លើយ
+                    <Link to={"/options"}>
+                        <a className="btn btn-primary btn-md text-md">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 10.5V6.75a4.5 4.5 0 1 1 9 0v3.75M3.75 21.75h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H3.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3" />
                             </svg>
-                        </button>
+                            ត្រឡប់
+                        </a>
+                    </Link>
+                    <button onClick={() => setShowAnswer(true)} className="btn btn-success btn-ghost btn-outline text-white">
+                        ចម្លើយ
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 10.5V6.75a4.5 4.5 0 1 1 9 0v3.75M3.75 21.75h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H3.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
+                        </svg>
+                    </button>
                     {/* </div> */}
                 </div>
-
+                {/* <div className="text-white flex justify-center"> */}
+                    <div className="flex justify-center text-white">
+                        <svg height={33} className="mr-0.5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5" />
+                        </svg>
+                        <h1 className="text-3xl ml-1">
+                            {
+                                getTeacherName(filteredQuestions[currentIndex].teacher)
+                            }
+                        </h1>
+                    </div>
+                    <div className="flex justify-center text-white mt-5">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25" />
+                        </svg>
+                        <h1 className="text-xl ml-1">
+                            {
+                                getBible(selectedType)
+                            }
+                        </h1>
+                    </div>
+                {/* </div> */}
                 <div className="hero bg-gray-800 mt-16">
                     <div className="hero-content text-center text-white text-xl">
                         <div className="max-w-md">
